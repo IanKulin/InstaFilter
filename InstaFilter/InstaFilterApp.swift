56eb164eee561169f9a69c14b51ac928c947a35a
Ian Bailey <IanKulin@kulin.com.au>
2022-11-28 18:12:52 +0800

    Init

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Ian Bailey on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
